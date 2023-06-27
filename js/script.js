66f4d52afd7e68302ad732ebff975dcdbb58d1f5
armanpinachyan <pinachyan99@mail.ru>
2023-06-27 16:04:44 +0400

    16:04

--- js/script.js
+++ js/script.js
@@ -12,20 +12,31 @@ const { none, show, active, hidden, opacity25, disabled } = {
 
 
 class ToolMasks {
-    static numberMaskElements = document.querySelectorAll('.def-fields-input');
+    static numberMaskElements = document.querySelectorAll('.mask-phone');
+    static maskOneKeyElements = document.querySelectorAll('.mask-one-key');
 
     constructor() {
     }
 
 
     static russianPhoneMask(){
-        this.numberMaskElements.forEach((item) => IMask(item, {
-            mask: '+{7}(000)000-00-00',
+        this.numberMaskElements.forEach((item) => this.startMask(item, '+{7}(000)000-00-00'))
+    }
+
+    static maskOneKey(){
+        this.maskOneKeyElements.forEach((item) => this.startMask(item,  '000 - 000'))
+
+    }
+
+    static startMask(item, mask){
+        return IMask(item, {
+            mask: mask,
             lazy: false,
             placeholderChar: '0'
-        }))
+        })
     }
 
 }
 
 ToolMasks.russianPhoneMask()
+ToolMasks.maskOneKey()
