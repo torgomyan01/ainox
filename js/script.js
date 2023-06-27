420a1f449245bc43b384e48b9f02d24dc95f9337
torgomyan1998 <andranik.torgomyan1998@gmail.com>
2023-06-27 15:10:23 +0400

    Add input mask js

--- js/script.js
+++ js/script.js
@@ -6,61 +6,26 @@ const { none, show, active, hidden, opacity25, disabled } = {
     opacity25: 'opacity-25',
     disabled: 'disabled'
 }
-const img_widget = document.querySelectorAll('.image-widget')
-const box_item = document.querySelectorAll('.box-item')
 
-const { mobileMenu, body, imageWidget } = {
-    mobileMenu: $('.mobile-menu'),
-    body: $('body'),
-    imageWidget: $('.image-widget'),
-}
 
-AOS.init()
 
-$('.mobile-menu-board').on('click', function (){
-    const thisElem = $(this);
-    if(thisElem.hasClass(active)){
-        mobileMenu.removeClass(active)
-        thisElem.removeClass(active)
-        body.removeClass(hidden)
-    } else {
-        mobileMenu.addClass(active)
-        thisElem.addClass(active)
-        body.addClass(hidden)
+
+
+class ToolMasks {
+    static numberMaskElements = document.querySelectorAll('.default-phone-input');
+
+    constructor() {
     }
-})
 
 
-function clearActive(){
-    img_widget.forEach(im=>{
-        im.classList.remove('active')
-    })
+    static russianNumberMask(){
+        this.numberMaskElements.forEach((item) => IMask(item, {
+            mask: '+{7}(000)000-00-00',
+            lazy: false,  // make placeholder always visible
+            placeholderChar: '0'     // defaults to '_'
+        }))
+    }
+
 }
 
-img_widget.forEach(el =>{
-    el.addEventListener('click', ()=>{
-        clearActive()
-        el.classList.add('active')
-    })
-})
-
-imageWidget.on('click', function (){
-    imageWidget.addClass(opacity25);
-    $(this).removeClass(opacity25)
-})
-
-const defaultInput = $('.def-input input');
-const inpLabel = $('.def-input');
-
-defaultInput.on('blur', function (){
-    inpLabel.map((index, e) => {
-        if($(e).children('input').val() === ''){
-            $(e).removeClass(active);
-        }
-    })
-})
-
-defaultInput.on('focus', function (){
-    $(this).parent().addClass(active);
-})
-
+ToolMasks.russianNumberMask()
