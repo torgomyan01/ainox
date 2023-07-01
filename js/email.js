e9d05ba7a17aec48ffb5cd5c20ecfc20cd26fbba
armanpinachyan <pinachyan99@mail.ru>
2023-07-01 14:39:17 +0400

    14:39

--- js/email.js
+++ js/email.js
@@ -7,7 +7,7 @@ btnEmaile.addEventListener('click', startValidation)
 
 
 function startValidation(){
-    btnEmaile.classList.add(btnLoading)
-    setTimeout(() => btnEmaile.classList.remove(btnLoading), 2000)
-  }
+  btnEmaile.classList.add(btnLoading)
+  setTimeout(() => btnEmaile.classList.remove(btnLoading), 2000)
+}
 
--- js/new-ticket.js
+++ js/new-ticket.js
@@ -4,14 +4,14 @@ const bottomLeft = document.querySelector('.new-request .botom-left')
 const exit_newRequest = document.querySelector('.new-request .exit-newRequest')
 const fields_new_ticket = document.querySelector('.technical-support .fields-new-ticket')
 const new_request = document.querySelector('.new-request')
+const form = document.querySelector('.new-request .form')
 const loading_btn = document.querySelector('.new-request .footer-buttons-btn')
 const loading_input = document.querySelector('.input-loading')
+const upload_box = document.querySelector('.upload-box')
+const upload_box_img = document.querySelector('.upload-box img')
+const todos = document.querySelector('.todos')
 
-
-loading_input.addEventListener('change', (e)=>{
-
-})
-
+//  btn loading
 
 loading_btn.addEventListener('click', startValidation)
 
@@ -21,8 +21,7 @@ function startValidation(){
   setTimeout(() => loading_btn.classList.remove(btnLoading), 2000)
 }
 
-
-
+// open modal
 
 bottomLeft.addEventListener('click', extiRequest)
 fields_new_ticket.addEventListener('click',  openRequest)
@@ -40,3 +39,58 @@ function extiRequest(){
 function closeRequest (){
   new_request.classList.add('close')
 }
+
+// file todos
+
+loading_input.addEventListener('change', (e)=>{
+    const todo_item = document.createElement('div')
+    todo_item.classList.add('todo-item')
+    todo_item.insertAdjacentHTML('beforeend', ` ${e.target.files[0].name}
+    <img class="fa-trash" src="assets/icons/fa-trash.svg" alt="trash">`)
+      todos.appendChild(todo_item)
+})
+
+todos.addEventListener('click', (e)=>{
+  let item = e.target
+  if(item.classList[0] === 'fa-trash'){
+    let todo = item.parentElement;
+      todo.remove()
+  }
+})
+
+
+
+// drag over
+
+upload_box.addEventListener('dragover', dragOverFunc)
+form.addEventListener('dragenter', dragEnterFunc)
+upload_box_img.addEventListener('dragleave', dragLeaveFunc)
+upload_box_img.addEventListener('drop', dropFunc)
+
+
+function dragOverFunc(event){
+  event.preventDefault()
+}
+
+function dragEnterFunc(e){
+ upload_box.classList.add(active)
+}
+
+function dragLeaveFunc(){
+  upload_box.classList.remove(active)
+}
+
+function dropFunc(e){
+  e.preventDefault();
+  e.stopPropagation();
+  console.log( e.dataTransfer.files)
+  upload_box.classList.remove(active)
+  const todo_item = document.createElement('div')
+  todo_item.classList.add('todo-item')
+  todo_item.insertAdjacentHTML('beforeend', ` ${e.dataTransfer.files[0].name}
+    <img class="fa-trash" src="assets/icons/fa-trash.svg" alt="trash">`)
+  todos.appendChild(todo_item)
+
+
+}
+
