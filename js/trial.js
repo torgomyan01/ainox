472991c3163fcb6e6af39ce3e71f1d28851bd6fb
armanpinachyan <pinachyan99@mail.ru>
2023-07-05 18:13:34 +0400

    18:13

--- js/trial.js
+++ js/trial.js
@@ -6,11 +6,12 @@ window.addEventListener('scroll', ()=>{
   if(scrollTop > 60){
     $element('.got-a-message').classList.add('active')
     $element('.ticket-message .navbar').classList.remove('actives')
-    $element('.ticket-message-description').classList.remove('actives')
+    $element('.trial-container').classList.remove('active')
   }
   else if(scrollTop === 0){
     $element('.got-a-message').classList.remove('active')
     $element('.ticket-message .navbar').classList.add('actives')
-    $element('.ticket-message-description').classList.add('actives')
+    $element('.trial-container').classList.add('active')
   }
 })
+
