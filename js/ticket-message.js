1396210bc89fd137433a8f0d7acdcb230f52eca9
armanpinachyan <pinachyan99@mail.ru>
2023-07-14 01:23:52 +0400

    01:23

--- js/ticket-message.js
+++ js/ticket-message.js
@@ -58,72 +58,73 @@ function startValidation(){
   setTimeout(() =>{
     btn_gray.classList.remove('loading-file-btn')
     wrappers.insertAdjacentHTML('beforeend', ` 
-      <div class="wrapper wrapper-ticket-message" data-type="no-read">
-        <div class="buttons-wrapper">
-          <button class="btn-status no-read ">
-            <i class="icon" data-name="notification"></i>
-            Не прочитано
-          </button>
-          <a href="#">
-            <img src="assets/icons/check-mark.svg" alt="chek-mark">
-            CS-999999999
-          </a>
-        </div>
-        <a href="ticket.html">
+           <a href="ticket.html">
+        <div class="wrapper wrapper-ticket-message" data-type="no-read">
+          <div class="buttons-wrapper">
+            <button class="btn-status no-read ">
+              <i class="icon" data-name="notification"></i>
+              Не прочитано
+            </button>
+            <span>
+              <img src="assets/icons/check-mark.svg" alt="chek-mark">
+              CS-999999999
+            </span>
+          </div>
           <h4 class="wrapper-title">
             Как я могу оформить договор и платить вам еще больше денег если у меня миллион подписчиков?
           </h4>
-        </a>
-        <p class="wrapper-subtitle">Обновлено 21 мая 2023 г. в 10:11</p>
-      </div>
-      <div class="wrapper wrapper-ticket-message" data-type="wait">
-        <div class="buttons-wrapper">
-          <button class="btn-status wait ">
-            <i class="icon" data-name="loading"></i>
-            Ожидает ответ
-          </button>
-          <a href="#">
-            <img src="assets/icons/check-mark.svg" alt="chek-mark">
-            CS-999999999
-          </a>
+
+          <p class="wrapper-subtitle">Обновлено 21 мая 2023 г. в 10:11</p>
         </div>
-        <a href="ticket.html">
-          <h4 class="wrapper-title-min">Настройка интеграции с сайтом</h4>
-        </a>
-        <p class="wrapper-subtitle-min">Обновлено 21 мая 2023 г. в 10:11</p>
-      </div>
-      <div class="wrapper wrapper-ticket-message" data-type="read-it">
-        <div class="buttons-wrapper">
-          <button class="btn-status read-it ">
-            <i class="icon" data-name="view"></i>
-            Прочитано
-          </button>
-          <a href="#">
-            <img src="assets/icons/check-mark.svg" alt="chek-mark">
-            CS-999999999
-          </a>
+      </a>
+      <a href="ticket.html">
+        <div class="wrapper wrapper-ticket-message" data-type="wait">
+          <div class="buttons-wrapper">
+            <button class="btn-status wait ">
+              <i class="icon" data-name="loading"></i>
+              Ожидает ответ
+            </button>
+            <span>
+              <img src="assets/icons/check-mark.svg" alt="chek-mark">
+              CS-999999999
+            </span>
+          </div>
+            <h4 class="wrapper-title-min">Настройка интеграции с сайтом</h4>
+          <p class="wrapper-subtitle-min">Обновлено 21 мая 2023 г. в 10:11</p>
         </div>
-        <a href="ticket.html">
-          <h4 class="wrapper-title-min">Не работает отправка уведомлений о том, что я не читаю инструкции</h4>
-        </a>
-        <p class="wrapper-subtitle-min">Обновлено 21 мая 2023 г. в 10:11</p>
-      </div>
-      <div class="wrapper wrapper-ticket-message" data-type="closed">
-        <div class="buttons-wrapper">
-          <button class="btn-status closed ">
-            <i class="icon" data-name="check"></i>
-            Закрыто
-          </button>
-          <a href="#">
-            <img src="assets/icons/check-mark.svg" alt="chek-mark">
-            CS-999999999
-          </a>
+      </a>
+      <a href="ticket.html">
+        <div class="wrapper wrapper-ticket-message" data-type="read-it">
+          <div class="buttons-wrapper">
+            <button class="btn-status read-it ">
+              <i class="icon" data-name="view"></i>
+              Прочитано
+            </button>
+            <span>
+              <img src="assets/icons/check-mark.svg" alt="chek-mark">
+              CS-999999999
+            </span>
+          </div>
+            <h4 class="wrapper-title-min">Не работает отправка уведомлений о том, что я не читаю инструкции</h4>
+          <p class="wrapper-subtitle-min">Обновлено 21 мая 2023 г. в 10:11</p>
         </div>
-        <a href="ticket.html">
-          <h4 class="wrapper-title-min">Настройка интеграции с сайтом</h4>
-        </a>
-        <p class="wrapper-subtitle-min">Обновлено 21 мая 2023 г. в 10:11</p>
-      </div>
+      </a>
+      <a href="ticket.html">
+        <div class="wrapper wrapper-ticket-message" data-type="closed">
+          <div class="buttons-wrapper">
+            <button class="btn-status closed ">
+              <i class="icon" data-name="check"></i>
+              Закрыто
+            </button>
+            <span>
+              <img src="assets/icons/check-mark.svg" alt="chek-mark">
+              CS-999999999
+            </span>
+          </div>
+            <h4 class="wrapper-title-min">Настройка интеграции с сайтом</h4>
+          <p class="wrapper-subtitle-min">Обновлено 21 мая 2023 г. в 10:11</p>
+        </div>
+      </a>
   `)
     startConvertIcons()
   },1500)
