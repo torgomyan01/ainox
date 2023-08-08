0811a7f2f601c516991d547bea955d00b151fa0d
Andranik Torgomyan <andranik@nteams.com>
2023-08-08 16:23:03 +0400

    Fix bugs

--- js/main.js
+++ js/main.js
@@ -136,6 +136,12 @@ ToolMasks.maskSerialPassport()
 ToolMasks.maskBik()
 ToolMasks.maskCheckingAccount()
 
+
+// First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+let vh = window.innerHeight * 0.01;
+// Then we set the value in the --vh custom property to the root of the document
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+
 // ------------- ************* ---------------------- //
 //                  CONSTANTS
 // ------------- ************* ---------------------- //
